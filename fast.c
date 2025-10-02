638dac62ddabb4b8d122014985ee1a6ebefef3ab
Siyam <pabnachild.ltd@gmail.com>
2025-10-02 00:40:37 -0400

    first commit

--- fast.c
+++ fast.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 int main() {
+    // here is i takeing only variables name only
     int a, b, sum;
 
-    printf("Enter First Number: ");
+    // Here I asking Valu from user
+    printf("Enter First Number: "); 
     scanf("%d", &a);
     
     printf("Enter Second Number: ");
     scanf("%d", &b);
     
+    // now here i calculate a+b
     sum = a + b;
-    printf("sum = %d\n", sum);  // Use %d to print an integer
+    printf("sum = %d\n", sum);  
+    
+    // Use %d to print an integer and use \n to keep a new line
 
     return 0;
 }
